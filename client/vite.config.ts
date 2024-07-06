5027455948259f1a9252e05878d2ba907bac742e
KienQuan <quan.htk>
2024-07-06 16:07:40 +0700

    vite conffig

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   port: 5173, // Thay đổi cổng này thành cổng bạn muốn sử dụng
-  //   open: true, // Tùy chọn: Tự động mở trình duyệt khi máy chủ khởi động
-  //   host: '103.82.194.250' // Tùy chọn: Cho phép truy cập từ mạng ngoài
-  // }
+  server: {
+    port: 5173, // Thay đổi cổng này thành cổng bạn muốn sử dụng
+    open: true, // Tùy chọn: Tự động mở trình duyệt khi máy chủ khởi động
+    host: '103.82.194.250' // Tùy chọn: Cho phép truy cập từ mạng ngoài
+  }
 })
